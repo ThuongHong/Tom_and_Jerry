1d64343fb90baa0c26322122c11350c0ae57c9f8
unknown <phamhoa23us@gmail.com>
2024-05-05 00:03:22 +0700

    fix: Change input for function BDFS in solve_maze

--- solving_maze/solving_maze.py
+++ solving_maze/solving_maze.py
@@ -11,9 +11,17 @@ def solve_maze(player: Character, algorithm: str = 'DFS') -> list[tuple[str, tup
     Returns:
         list[tuple[str, tuple[int]]]: A list of move, e.g [('L', (3, 3)), ('R', (4, 3))]
     """
+    player_current_position = player.position
+    
+    maze = player.Maze
+
+    player_winning_position = maze.end_position
 
     if algorithm == 'DFS' or algorithm == 'BFS':
-        return BDFS(player=player,algorithm=algorithm)
+        return BDFS(grids= maze.grids, 
+                    player_current_position= player_current_position, 
+                    player_winning_position= player_winning_position, 
+                    algorithm=algorithm)
     elif algorithm == 'GDFS':
         raise NotImplementedError
     elif algorithm == 'AStar':
