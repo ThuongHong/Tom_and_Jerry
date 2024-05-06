ed5d83031ddc87fc00909c59f6e77c47ddc0a861
ChungDat <chungdatk5@gmail.com>
2024-05-06 21:25:30 +0700

    perf: mimic real game button

--- menu_objects/button.py
+++ menu_objects/button.py
@@ -8,37 +8,35 @@ class Button:
         self.modified_height = int(height * scale)
         self.modified_hover_width = int(width * hover_scale)
         self.modified_hover_height = int(height * hover_scale)
+        self.hover_x_coord = int(x_coord - (self.modified_hover_width - self.modified_width) / 2)
         
         self.sound = sound
         self.image = pygame.transform.scale(image, (self.modified_width, self.modified_height))
         self.hover_image = pygame.transform.scale(image, (self.modified_hover_width, self.modified_hover_height))
         self.rect = self.image.get_rect()
         self.rect.topleft = (x_coord, y_coord)
-        self.clicked = False
         self.mouse_down = False
         self.mouse_click = False
-                
+    
     def draw(self, surface):
         action = False
-        # get mouse postition
         pos = pygame.mouse.get_pos()
         
         # check mouseover and clicked conditions
         if self.rect.collidepoint(pos):
-            hover_width = self.hover_image.get_width()
-            hover_x_coord = int(self.rect.x - (hover_width - self.modified_width) / 2)
-            # draw button on screen
-            surface.blit(self.hover_image, (hover_x_coord, self.rect.y))
-            if pygame.mouse.get_pressed()[0] == 1:
-                self.mouse_down = True
-            if pygame.mouse.get_pressed()[0] == 0 and self.mouse_down == True:
-                self.mouse_click = True
+            if self.mouse_click == False:
+                surface.blit(self.hover_image, (self.hover_x_coord, self.rect.y))
+                for event in pygame.event.get():
+                    pos = pygame.mouse.get_pos()
+                    if event.type == pygame.MOUSEBUTTONDOWN:
+                        self.mouse_down = True
+                    if event.type == pygame.MOUSEBUTTONUP and self.mouse_down == True:
+                        self.mouse_click = True
+                
         else:
-            # draw button on screen
             surface.blit(self.image, (self.rect.x, self.rect.y))
-            if pygame.mouse.get_pressed()[0] == 0:
-                self.mouse_down = False
-            
+            self.mouse_down = False
+        
         if self.mouse_click == True:
             pygame.mixer.Sound.play(self.sound)
             action = True
