a0d1d216ed799c4a84d123093901ee048ed9712b
PhamPhuHoa-23 <phamhoa23us@gmail.com>
2024-05-10 22:44:39 +0700

    feat: Update data.py using SQL | All code is comment because interface does not have database

--- data/data.py
+++ data/data.py
@@ -1,4 +1,6 @@
 import json
+import sqlite3 # Using this later. May be when merge to develop :>>.
+from werkzeug.security import check_password_hash, generate_password_hash
 
 def register(new_username: str, new_password: str):
     with open("data/user_accounts.json", "r", encoding="utf-8") as f:
@@ -20,6 +22,24 @@ def register(new_username: str, new_password: str):
 
     return register_flag
 
+    # # FUHOA UPDATE USING DATABASE
+    # db_connect = sqlite3.connect(r'database/TomJerry.db')
+
+    # db_cursor = db_connect.cursor()
+
+    # # Check name in Database
+    # is_name_in_database = db_cursor.execute('SELECT * FROM "users WHERE "username" = ?', (new_username))
+
+    # if is_name_in_database: 
+    #     register_flag = False
+    #     return register_flag
+    
+    # # Check name not in Database
+    # hash_password = generate_password_hash(new_password)
+
+    # db_cursor.execute('INSERT INTO "users"("username", "password") VALUES(?, ?)', (new_username, hash_password))
+
+    # db_connect.commit()
 
 def login(username: str, password: str):
     with open("data/user_accounts.json", "r", encoding="utf-8") as f:
@@ -32,6 +52,16 @@ def login(username: str, password: str):
             break
 
     return login_flag
+    
+    # # FUHOA UPDATE USING DATABASE
+    # db_connect = sqlite3.connect(r'database/TomJerry.db')
 
+    # db_cursor = db_connect.cursor()
 
+    # # CHECK IF THE USERS EXIST
+    # is_user_exist = list(db_cursor.execute('SELECT "password" FROM "users" WHERE "username" = ?', (username)))
 
+    # if is_user_exist:
+    #     if check_password_hash(pwhash= is_user_exist[0][0], password= password):
+    #         login_flag = True
+    #         return login_flag
