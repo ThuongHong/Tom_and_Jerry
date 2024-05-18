fc882d4f55fb95b1b6e50903e8603d8d6c2564ae
ChungDat <chungdatk5@gmail.com>
2024-05-18 22:56:06 +0700

    feat: choose spawn point

--- menu_objects/graphic.py
+++ menu_objects/graphic.py
@@ -4,6 +4,8 @@ class Graphic:
     def __init__(self, x_coord, y_coord, image, scale=1):
         width = image.get_width()
         height = image.get_height()
+        self.x_coord = x_coord
+        self.y_coord = y_coord
         self.modified_width = int(width * scale)
         self.modified_height = int(height * scale)
         self.image = pygame.transform.scale(image, (self.modified_width, self.modified_height))
